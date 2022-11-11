ba8fd5fdbfc4a26a1a5990588ca7cbb0c92faf2c
bioinformatician.pm <104200282+Bioinformatician-Club@users.noreply.github.com>
2022-11-11 14:50:08 +0100

    update example.R

--- example/example.R
+++ example/example.R
@@ -4,21 +4,25 @@ check()
 
 data("1000g")
 
-# function 1 based on maidou version
+# generate allele count table for each gene and each sample
 gene.ac <- count_vars_per_gene_per_sample(table = variants, gene.col = 8, gt.col = 12, type = "allele")
 
+# generate the table of allele carrier status for each gene and each sample
 gene.carrier <- count_vars_per_gene_per_sample(table = variants, gene.col = 8, gt.col = 12, type = "carrier")
 
-# function 2 based on xiaoyi version
+# here just pick some genes and use as an example as a gene set input
 gene.list <- gene.ac$GENE[10:14]
 
+# count the total number of alleles in the gene set for each sample
 geneset.ac <- count_vars_in_gene_set_per_sample(table = gene.ac, gene.set = gene.list, gene.set.name = "genesetA" )
 
+# count the total number of genes carrying at least one variant in the gene set for each sample
 geneset.carrier <- count_vars_in_gene_set_per_sample(table = gene.carrier, gene.set = gene.list, gene.set.name = "genesetA" )
 
-# function 3
-# the input ped must be set with correct class/type for each variable. e.g. outcome/y/dependent variable must be a binary variable. Some of independent variables need to be set in proper type, for instance, sex variable normally should be set to factor.
-
+# the input ped must be assigned with correct class/type for each variable. e.g.
+# * the outcome/dependent variable must be a binary variable, either 0 or 1.
+# * according to the natural property of some independent variables, they may need to be assigned with a proper class/type. e.g. usually the variable representing sex should be assigned as a factor.
+#
 ped.with.geneset.count <- ped %>%
   as_tibble() %>%
   left_join(geneset.ac, by = c("IND_ID" = "sampleID")) %>%
@@ -26,9 +30,9 @@ ped.with.geneset.count <- ped %>%
   mutate(DISEASE = DISEASE - 1,
          SEX = factor(SEX))
 
+
 # outcome variable: it must be a binary variable. The value should be either 0 or 1.
 # interested.variables : all variables here must have the same class/type. e.g. genesetA and AGE are both numeric types in our example here. If you add SEX to interested.variables, you will get an error. Because SEX is factor.
-
 glm.results.tidy <- glm_for_multiple_tests(ped = ped.with.geneset.count, outcome = "DISEASE", interested.variables = c("genesetA", "AGE"), covariates= c("QT","SEX"), tidy.output = TRUE, conf.int = TRUE, exponentiate = F)
 
 glm.results.tidy
